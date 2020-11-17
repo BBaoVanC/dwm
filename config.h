91fd3f44294e27a8ac8c3205b1031297a782fd07
BBaoVanC <github@bbaovanc.com>
2020-11-17 16:59:26 -0600

    Set up nord theme

--- config.h
+++ config.h
@@ -11,14 +11,14 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMono Nerd Font:style=Bold:size=10" };
 static const char dmenufont[]       = "JetBrainsMono Nerd Font:style=Bold:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#2e3440";
+static const char col_gray2[]       = "#434c5e";
+static const char col_gray3[]       = "#d8dee9";
+static const char col_gray4[]       = "#eceff4";
+static const char col_cyan[]        = "#81a1c1";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
@@ -31,7 +31,6 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class		instance	title		tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",		NULL,		NULL,		0,         1,          0,           0,        -1 },
 	{ "Firefox",		NULL,		NULL,		1 << 8,    0,          0,          -1,        -1 },
 	{ "st",			NULL,		NULL,		0,         0,          1,           0,        -1 },
 	{ "Thunderbird",	"Msgcompose", 	NULL,		0,         1,          0,           0,        -1 },
