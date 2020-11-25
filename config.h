093851f3d6822381b99c47528edc5c04c0f7d6b0
BBaoVanC <github@bbaovanc.com>
2020-11-24 19:47:21 -0600

    Enable color emoji support

--- config.h
+++ config.h
@@ -13,8 +13,7 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:style=Bold:size=10" };
-static const char dmenufont[]       = "JetBrainsMono Nerd Font:style=Bold:size=10";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:style=Bold:size=10", "JoyPixels:pixelsize=10" };
 static const char col_gray1[]       = "#2e3440";
 static const char col_gray2[]       = "#434c5e";
 static const char col_gray3[]       = "#d8dee9";
@@ -70,7 +69,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *firefoxcmd[]  = { "firefox-nightly", NULL };
 static const char *discordcanarycmd[]  = { "discord-canary", NULL };
